887f4e93c1da1a83429bbd00f0f5a5051b48e0c8
bvviet <lanviethoang2004@thoang2004gmail.g2004gmailcom>
2024-06-11 08:20:38 +0700

    lan4

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import router from "./src/routes/index.js";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 const app = express();
 const port = 3000;
+dotenv.config();
 
 app.use(express.json());
 app.use(
@@ -14,6 +16,7 @@ app.use(
 
 app.use(cors());
 
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/angular";
 // Hoặc cấu hình cors chi tiết hơn
 app.use(
     cors({
@@ -30,14 +33,13 @@ app.get("/", (req, res) => {
 
 app.use("/", router);
 
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
 
 const connect = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/angular");
+        await mongoose.connect(dbUrl);
         console.log("connected");
     } catch (error) {
         console.error("Error connecting to MongoDB", error);
